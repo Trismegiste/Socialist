ee06faeaeb3ee2d3e580e4a95d420fcb9a22d0c0
Trismegiste <flo@trismegiste.net>
2014-08-13 00:14:05 +0200

    abstract model ok

---
+++ tests/model/CommentaryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Socialist
+ */
+
+namespace tests\model;
+
+use Trismegiste\Socialist\Commentary;
+
+/**
+ * CommentaryTest tests Commentary entity
+ */
+class CommentaryTest extends ContentTest
+{
+
+    protected function createSUT()
+    {
+        return new Commentary($this->mockAuthor);
+    }
+
+    public function getMessage()
+    {
+        return [['warp fator 1']];
+    }
+
+    /**
+     * @dataProvider getMessage
+     */
+    public function testMessage($str)
+    {
+        $this->sut->setMessage($str);
+        $this->assertEquals($str, $this->sut->getMessage());
+        $this->assertNotEquals('sghsjgcsg', $this->sut->getMessage());
+    }
+
+}
