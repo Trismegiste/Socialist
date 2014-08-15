7c3b7951dd997f506c3ed898eacd9a10b56dfe62
Trismegiste <flo@trismegiste.net>
2014-08-15 10:44:02 +0200

    add a contract for reporting abusive content

---
+++ src/Trismegiste/Socialist/AbusiveReport.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Socialist
+ */
+
+namespace Trismegiste\Socialist;
+
+/**
+ * AbusiveReport is a contract for reporting and counting if a content
+ * is abusive/offensive...
+ */
+interface AbusiveReport
+{
+    // not implemented yet
+}
--- src/Trismegiste/Socialist/Content.php
+++ src/Trismegiste/Socialist/Content.php
@@ -9,13 +9,15 @@ namespace Trismegiste\Socialist;
 /**
  * Content is a content posted on the net by an AuthorInterface
  */
-abstract class Content implements Famous
+abstract class Content implements Famous, AbusiveReport
 {
 
     use FamousImpl;
 
     protected $author;
     protected $lastEdited;
+
+    // array of Author for abusive content goes here
 
     public function __construct(AuthorInterface $auth)
     {
