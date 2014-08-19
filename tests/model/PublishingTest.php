fb8bce1e58a0364b4f169c5e2324928b1cd23fc5
Trismegiste <flo@trismegiste.net>
2014-08-20 01:31:56 +0200

    missing CC and unit tests

--- tests/model/PublishingTest.php
+++ tests/model/PublishingTest.php
@@ -33,12 +33,18 @@ class PublishingTest extends ContentTest
 
     public function testAttachCommentary()
     {
+        $this->assertCount(0, $this->sut->getCommentary());
         $this->sut->attachCommentary($this->message);
+        $this->assertCount(1, $this->sut->getCommentary());
     }
 
     public function testDetachCommentary()
     {
+        $this->assertCount(0, $this->sut->getCommentary());
         $this->sut->attachCommentary($this->message);
+        $this->assertCount(1, $this->sut->getCommentary());
+        $this->sut->detachCommentary($this->message);
+        $this->assertCount(0, $this->sut->getCommentary());
         $this->sut->detachCommentary($this->message);
     }
 
