58cfe0446bf970d9b17dc0c3b618bcd4158229e9
Trismegiste <flo@trismegiste.net>
2014-09-21 21:35:56 +0200

    checking precision

--- tests/database/StatusTest.php
+++ tests/database/StatusTest.php
@@ -20,8 +20,8 @@ class StatusTest extends PublishingTestCase
     {
         $sut = new Status($author);
         $sut->setMessage("Hello there !");
-        $sut->setLatitude(43.00001);
-        $sut->setLongitude(7.00001);
+        $sut->setLatitude(43.000001);
+        $sut->setLongitude(7.000001);
 
         return $sut;
     }
@@ -29,8 +29,8 @@ class StatusTest extends PublishingTestCase
     protected function assertRootEquals(Publishing $doc)
     {
         $this->assertEquals("Hello there !", $doc->getMessage());
-        $this->assertEquals(43.00001, $doc->getLatitude());
-        $this->assertEquals(7.00001, $doc->getLongitude());
+        $this->assertEquals(43.000001, $doc->getLatitude());
+        $this->assertEquals(7.000001, $doc->getLongitude());
     }
 
 }
