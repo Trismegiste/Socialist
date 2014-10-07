7719a1c419fe636a4801b98d5cdb56fe1e69ea41
Trismegiste <flo@trismegiste.net>
2014-10-08 01:10:29 +0200

    adding model of picture

---
+++ src/Picture.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * Socialist
+ */
+
+namespace Trismegiste\Socialist;
+
+/**
+ * Picture is a published picture with a text
+ */
+class Picture extends SmallTalk
+{
+
+    /**
+     * @var string the object's key in the storage system (no assumption)
+     */
+    protected $storageKey;
+
+    /**
+     * @var string the mime-type of this picture
+     */
+    protected $mimeType = 'image/*';
+
+    /**
+     * set the storage key of this picture
+     * For optimizing perf, this key must start with randomness
+     * like md5($nickname . time()) . '-' . $filename
+     *
+     * @param string $key
+     */
+    public function setStorageKey($key)
+    {
+        $this->objectKey = $key;
+    }
+
+    /**
+     * get the storage key of this picture
+     *
+     * @return string $key
+     */
+    public function getStorageKey()
+    {
+        return $this->objectKey;
+    }
+
+    /**
+     * set the mime-type of this picture
+     *
+     * @param string $str A string like "image/jpeg" or "image/png"
+     */
+    public function setMimeType($str)
+    {
+        $this->mimeType = $str;
+    }
+
+    /**
+     * Get the mime-type
+     *
+     * @return string
+     */
+    public function getMimeType()
+    {
+        return $this->mimeType;
+    }
+
+}
