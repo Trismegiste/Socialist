f9d997787d519a81fd1abc287737a675c40f0b7d
Trismegiste <flo@trismegiste.net>
2014-10-08 01:20:01 +0200

    test of picture model

---
+++ tests/model/PictureTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Socialist
+ */
+
+namespace tests\model;
+
+use Trismegiste\Socialist\Picture;
+
+/**
+ * PictureTest tests PictureTest
+ */
+class PictureTest extends PublishingTest
+{
+
+    protected function createSUT()
+    {
+        return new Picture($this->mockAuthorInterface);
+    }
+
+    public function getFixtures()
+    {
+        return [['ncc1701.jpg', 'image/jpeg', 'A fine ship']];
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testProperties($filename, $mime, $msg)
+    {
+        $this->sut->setMessage($msg);
+        $this->assertEquals($msg, $this->sut->getMessage());
+        $this->assertNotEquals('garbage', $this->sut->getMessage());
+
+        $this->sut->setMimeType($mime);
+        $this->assertEquals($mime, $this->sut->getMimeType());
+        $this->assertNotEquals('adobe/pdf', $this->sut->getMimeType());
+
+        $this->sut->setStorageKey($filename);
+        $this->assertEquals($filename, $this->sut->getStorageKey());
+        $this->assertNotEquals('x-wing.png', $this->sut->getStorageKey());
+    }
+
+}
